6a4731e877bd710219c6b34f98604ad32f2c7da3
Abhishek Manral <abhishek.manral2021@vitbhopal.ac.in>
2023-12-14 21:59:16 +0530

    initial-commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from 'react-dom/client';
+import ReactMapboxGl from "react-mapbox-gl";
+import DrawControl from "react-mapbox-gl-draw";
+import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import "./styles.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Map = ReactMapboxGl({
+  accessToken:
+    "pk.eyJ1IjoiZmFrZXVzZXJnaXRodWIiLCJhIjoiY2pwOGlneGI4MDNnaDN1c2J0eW5zb2ZiNyJ9.mALv0tCpbYUPtzT7YysA2g"
+});
+
+function App() {
+  const onDrawCreate = ({ features }) => {
+    console.log(features);
+    const Coords = [];
+    let totalCoordinates = 0;
+  
+    for (const feature of features) {
+      const featureCoordinates = feature.geometry.coordinates;
+      Coords.push(featureCoordinates);
+      totalCoordinates += featureCoordinates.length;
+    }
+  
+    let LongitudeSum = 0;
+    let LatitudeSum = 0;
+  
+    Coords.forEach((item) => {
+      for (const coordinates of item) {
+        LongitudeSum += coordinates[0];
+        LatitudeSum += coordinates[1];
+      }
+    });
+  
+    const avgLongitude = LongitudeSum / totalCoordinates;
+    const avgLatitude = LatitudeSum / totalCoordinates;
+  
+    console.log(avgLongitude);
+    console.log(avgLatitude);
+  };
+  
+  const onDrawUpdate = ({ features }) => {
+    alert("onUpdateisCalled");
+    console.log(features);
+  };
+
+  return (
+      <Map
+        style="mapbox://styles/mapbox/streets-v9" 
+        containerStyle={{
+          height: "600px",
+          width: "100vw"
+        }}
+      >
+        <DrawControl onDrawCreate={onDrawCreate} onDrawUpdate={onDrawUpdate} />
+      </Map>
+
+  );
+}
+
+
+const root = createRoot(document.getElementById("root")); // createRoot(container!) if you use TypeScript
+root.render(<App />);
